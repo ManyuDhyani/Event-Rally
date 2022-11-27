a96efdecedb92ae3b95e9ba37cfc92878186d661
ManyuDhyani <manyudhyani@gmail.com>
2022-11-26 22:58:28 -0500

    Adding Config files for DB connection

---
+++ config/mongoCollections.js
@@ -0,0 +1,27 @@
+const dbConnection = require('./mongoConnection');
+
+/* This will allow you to have one reference to each collection per app */
+/* Feel free to copy and paste this this */
+const getCollectionFn = (collection) => {
+  let _col = undefined;
+
+  return async () => {
+    if (!_col) {
+      const db = await dbConnection.dbConnection();
+      _col = await db.collection(collection);
+    }
+
+    return _col;
+  };
+};
+
+/* Now, you can list your collections here: 
+NOTE: YOU WILL NEED TO CHANGE THE CODE BELOW TO HAVE THE COLLECTION(S) REQUIRED BY THE ASSIGNMENT */
+module.exports = {
+  users: getCollectionFn('users'),
+  profile: getCollectionFn('profile'),
+  event: getCollectionFn('event'),
+  comment: getCollectionFn('comment'),
+  followers: getCollectionFn('followers'),
+  report: getCollectionFn('report'),
+};
