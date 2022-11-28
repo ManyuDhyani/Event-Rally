7ffad473a877ee01e9344d8835b19127749ab218
ManyuDhyani <manyudhyani@gmail.com>
2022-11-28 18:06:14 -0500

    Implemented the like data function for event collection

---
+++ data/likes.js
@@ -0,0 +1,46 @@
+const mongoCollections = require('../config/mongoCollections');
+const validationFunctions = require('../validation');
+const events = mongoCollections.event;
+let { ObjectId } = require('mongodb');
+  
+const createlike = async (eventId, userId, value) => {
+    // Validation
+
+    eventId = eventId.trim();
+    userId = userId.trim();
+    value = value.trim();
+
+    // Current timestamp
+    timestamp = new Date().toUTCString();
+    
+    let eventsCollection = await events();
+
+    // Creating new like object
+    let newLike = {
+        user_id: userId,
+        value: value,
+        timestamp: timestamp
+    }
+
+    let event = await eventsCollection.findOne({_id: ObjectId(eventId)})
+    if (!event) {
+        throw {statusCode: 404, error:`No such event with id: ${eventId}`}
+    }
+
+    let insertLike = await eventsCollection.updateOne({_id:ObjectId(eventId)}, {$addToSet: {likes: newLike}});
+    if (insertLike.modifiedCount === 0) {
+        throw {statusCode: 500, error: "Internal Server Error: Unable to add the Like to the Event"};
+    }
+
+    let getUpdatedEvent = await eventsCollection.findOne({_id: ObjectId(eventId)});
+    if(!getUpdatedEvent){
+      throw {statusCode: 404, error: "Unable to get event after adding the Like"};
+    }
+
+    // Recalculate the likes and dislikes for the event and return.
+    
+};
+
+module.exports = {
+    createlike
+};
