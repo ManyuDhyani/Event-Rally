f20d5dfd33958a7b5a4c7835145dc75860dd28fb
ManyuDhyani <manyudhyani@gmail.com>
2022-12-07 18:56:57 -0500

    Implemented the get comment function for event

--- data/comments.js
+++ data/comments.js
@@ -2,30 +2,35 @@ const mongoCollections = require('../config/mongoCollections');
 const validationFunctions = require('./validation');
 const comments = mongoCollections.comment;
 
-const createComment = async (parentComment, content) => {
+const createComment = async (userId, eventId, parentCommentId, content) => {
     // validation
     // NOT NULL,string, lenght not zero AFTERN TRIM, 200 WORDS 
-
-    validationFunctions.idValidator(parentComment);
+    validationFunctions.idValidator(userId);
+    validationFunctions.idValidator(eventId);
+    validationFunctions.idValidator(parentCommentId);
     validationFunctions.contentValidator(content);
 
-
-    parentComment = parentComment.trim();
+    // Cleaning: Triming data before saving
+    userId = userId.trim();
+    eventId = eventId.trim();
+    parentCommentId = parentCommentId.trim();
     content = content.trim();
 
     // Current timestamp
     timestamp = new Date().toUTCString();
 
     // If parentComment comes in empty, means this comment is a parent comment itself. So this comment's parent_comment_id will be null.
-    if (!parentComment) {
-        parentComment = null;
+    if (!parentCommentId) {
+        parentCommentId = null;
     }
 
     let commentsCollection = await comments();
 
     // Creating new comment object
     let newComment = {
-        parent_comment_id: parentComment,
+        author_id: userId,
+        event_id: eventId,
+        parent_comment_id: parentCommentId,
         content: content,
         timestamp: timestamp
     }
@@ -45,6 +50,35 @@ const createComment = async (parentComment, content) => {
     return getNewComment;
 };
 
+
+// This function get all comments for the particular event.
+const getAllEventParentComments = async (eventId) => {
+    // validation
+    validationFunctions.idValidator(eventId);
+
+    eventId = eventId.trim();
+
+    let commentsCollection = await comments();
+    let commentsList = await commentsCollection.find({$and: [{event_id: eventId}, {parent_comment_id: null}]});
+
+    return commentsList;
+};
+
+const getAllChildCommentsThread = async (parentCommentId) => {
+    // validation
+    validationFunctions.idValidator(parentCommentId);
+
+    parentCommentId = parentCommentId.trim();
+
+    let commentsCollection = await comments();
+    let commentsList = await commentsCollection.find({parent_comment_id: parentCommentId}).sort({"timestamp": -1});
+
+    return commentsList;
+};
+
+
 module.exports = {
-    createComment
+    createComment,
+    getAllEventParentComments,
+    getAllChildCommentsThread
 };
