578928c779fb886e22b36b9a69c3eac92c7595bc
ManyuDhyani <manyudhyani@gmail.com>
2022-12-12 23:53:15 -0500

    Added handlebars helper functions for Profile page

--- app.js
+++ app.js
@@ -2,17 +2,31 @@
 
 const express = require('express');
 const app = express();
+const path = require('path')
 const static = express.static(__dirname + '/public');
 
 const configRoutes = require('./routes');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
+const Handlebars = require('handlebars');
+Handlebars.registerHelper('eq', function(a, b, options) {
+  if (a == b) return options.fn(this)
+  else return options.inverse(this)
+});
 
 app.use('/public', static);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 
-app.engine('handlebars', exphbs.engine({defaultLayout: 'main'}));
+app.engine('handlebars', exphbs.engine({
+  defaultLayout: 'main',
+  helpers: {
+    checkListLengthZero: function(arr){
+      return arr.length == 0;
+    }
+  },
+  partialsDir: ['views/partials/']
+}));
 app.set('view engine', 'handlebars');
 
 app.use(session({
