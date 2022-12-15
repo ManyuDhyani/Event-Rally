dc77cf8e26983080541fe4896a3e874b885a809a
Ashish Kothadiya <chakravartikothadiya@gmail.com>
2022-12-15 15:23:23 -0500

    Added image upload functionality for events

--- routes/events.js
+++ routes/events.js
@@ -3,6 +3,21 @@ const router = express.Router();
 const data = require('../data');
 const eventData = data.events;
 const validationFunctions = data.validationFunctions
+const multer = require('multer');
+
+//image upload
+let storage = multer.diskStorage({
+  destination: function(req, file, cb){
+    cb(null,'./public/uploads/events');
+  },
+  filename:function(req,file,cb){
+    cb(null,file.fieldname+"_"+Date.now()+"_"+file.originalname);
+  },
+});
+
+let upload = multer({
+  storage: storage,
+}).single('thumbnail_1');
 
 
 router
@@ -13,7 +28,10 @@ router
     if (req.session.login){
       res.render("events/eventCreateForm", {title:"Create Event", is_authenticated: req.session.login.authenticatedUser, username: req.session.username, user: req.session.login.loggedUser});
     }
-    return res.render("userLogin", {title: "Login"});
+    else
+    {
+      return res.render("userLogin", {title: "Login"});
+    }
   } catch (e) {
     if (e.statusCode) {
       res.status(e.statusCode).render("error", {title: "Error", error404: true});
@@ -22,16 +40,21 @@ router
     }
   }
 })
-.post(async(req, res) => {
+.post(upload,async(req, res) => {
   // validation for req.body all fileds here
   try
   { 
     if (req.session.login){
-      await validationFunctions.eventObjValidator(0,null,req.session.login.loggedUser._id, req.body.title, req.body.overview, req.body.content, req.body.category, req.body.thumbnail_1, req.body.thumbnail_2, req.body.thumbnail_3, req.body.thumbnail_4, req.body.tags, req.body.location, req.body.price);
-      const createEvent = await eventData.createEvent(req.session.login.loggedUser._id, req.body.title, req.body.overview, req.body.content, req.body.category, req.body.thumbnail_1, req.body.thumbnail_2, req.body.thumbnail_3, req.body.thumbnail_4, req.body.tags, req.body.location, req.body.price);
+
+      const validate = await validationFunctions.eventObjValidator(0,null,req.session.login.loggedUser._id, req.body.title, req.body.overview, req.body.content, req.body.category, req.file.filename, req.body.thumbnail_2, req.body.thumbnail_3, req.body.thumbnail_4, req.body.tags, req.body.location, req.body.price);
+      const createEvent = await eventData.createEvent(req.session.login.loggedUser._id, req.body.title, req.body.overview, req.body.content, req.body.category, req.file.filename, req.body.thumbnail_2, req.body.thumbnail_3, req.body.thumbnail_4, req.body.tags, req.body.location, req.body.price);
+      
       res.redirect('/events/'+createEvent);
     }
-    return res.render("userLogin", {title: "Login"});
+    else
+    {
+      return res.render("userLogin", {title: "Login"});
+    }
   }catch (e) {
     if (e.statusCode) {
       res.status(e.statusCode).render("error", {title: "Error", error500: true});
