74a9f54fa772f3f9466b5b91280133ac35ec390f
ManyuDhyani <manyudhyani@gmail.com>
2022-12-14 19:37:24 -0500

    Validation Resolve

--- data/validation.js
+++ data/validation.js
@@ -230,6 +230,131 @@ const complaintValidator = async(complaint) => {
 }
 
 
+const pincodeValidator = async(num) => {
+    var num2 = Number.parseInt(num);
+    if (!num) {
+        throw 'Error: Pincode cannot be empty';
+    }
+
+    if (Number.isNaN(num2)) {
+        throw 'Error: Pincode value should be a numeric...String Given';
+    }
+    if (num.length === 0 || num.trim().length === 0) {
+        throw 'Error: Pincode Value cannot be an empty  or just spaces';
+    }
+
+    // string
+    // no alphabet
+
+};
+
+const ageValidator = async(num) => {
+    var num2 = Number.parseInt(num);
+    if (!num) {
+        throw 'Error: Age cannot be empty';
+    }
+
+    if (Number.isNaN(num2)) {
+        throw 'Error: Age should be a numeric';
+    }
+    if (num.length === 0 || num.trim().length === 0) {
+        throw 'Error: Age cannot be an empty  or just spaces';
+    }
+    if (num <= 13) {
+        throw 'Error: Age should be greater than 13';
+    }
+
+};
+
+const genderValidator = async(gender) => {
+    gender = gender.trim();
+
+    if (typeof gender !== "string") {
+        throw 'Error: Gender must be a string';
+    }
+
+    if (gender.length === 0 || gender.trim().length === 0) {
+        throw 'Error: Gender cannot be an empty string or just spaces';
+    }
+
+    if (gender !== "Male" && gender !== "Female" && gender !== "Transgender" && gender !== "NoNBinary") {
+        throw 'Error: Gender must be either Male, Female ,Transgender or Binary...Anything else will not accepted';
+    }
+
+};
+
+
+const firstNameValidator = async(str) => {
+    if (!str) {
+        throw 'Error: First Name cannot be empty';
+    }
+
+    if (typeof str !== "string") {
+        throw 'Error: First Name must be a string';
+    }
+    if (str.length === 0 || str.trim().length === 0) {
+        throw 'Error: First Name cannot be an empty string or just spaces';
+    }
+};
+
+const lastNameValidator = async(str) => {
+    if (!str) {
+        throw 'Error: Last Name cannot be empty';
+    }
+
+    if (typeof str !== "string") {
+        throw 'Error: Last Name must be a string';
+    }
+    if (str.length === 0 || str.trim().length === 0) {
+        throw 'Error: Last Name cannot be an empty string or just spaces';
+    }
+
+};
+
+const cityValidator = async(str) => {
+    if (!str) {
+        throw 'Error: City cannot be empty';
+    }
+
+    if (typeof str !== "string") {
+        throw 'Error: City must be a string';
+    }
+    if (str.length === 0 || str.trim().length === 0) {
+        throw 'Error: City cannot be an empty string or just spaces';
+    }
+
+};
+
+const stateValidator = async(str) => {
+    if (!str) {
+        throw 'Error: State cannot be empty';
+    }
+
+    if (typeof str !== "string") {
+        throw 'Error: State must be a string';
+    }
+    if (str.length === 0 || str.trim().length === 0) {
+        throw 'Error: State cannot be an empty string or just spaces';
+    }
+
+};
+
+const countryValidator = async(str) => {
+    if (!str) {
+        throw 'Error: Country cannot be empty';
+    }
+
+    if (typeof str !== "string") {
+        throw 'Error: Country must be a string';
+    }
+    if (str.length === 0 || str.trim().length === 0) {
+        throw 'Error: Country cannot be an empty string or just spaces';
+    }
+
+};
+
+
+
 module.exports = {
     idValidator,
     usernameValidator,
@@ -241,6 +366,13 @@ module.exports = {
     againstValidator,
     complaintValidator,
     contentValidator,
+    tagsSplitter,
+    firstNameValidator,
+    lastNameValidator,
+    cityValidator,
+    stateValidator,
+    countryValidator,
+    pincodeValidator,
     ageValidator,
-    tagsSplitter
+    genderValidator
 };
