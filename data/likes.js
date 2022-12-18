893f8b3c4c36df71ed24b74bafeff865caaa494d
Ashish Kothadiya <chakravartikothadiya@gmail.com>
2022-12-18 01:25:40 -0500

    Added ajax for likes dislikes and counts

--- data/likes.js
+++ data/likes.js
@@ -8,14 +8,13 @@ const createlike = async (eventId, userId, value) => {
 
     await validationFunctions.idValidator(eventId);
     await validationFunctions.idValidator(userId);
-    await validationFunctions.valueValidator(value);
+    //await validationFunctions.valueValidator(value);
     
+
     eventId = eventId.trim();
     userId = userId.trim();
     value = value.trim();
    
-
-
     // Current timestamp
     timestamp = new Date().toUTCString();
     
@@ -23,37 +22,59 @@ const createlike = async (eventId, userId, value) => {
 
     // Creating new like object
     let newLike = {
-        user_id: userId,
+        user_id: ObjectId(userId),
         value: value,
         timestamp: timestamp
     }
 
+    //Check if event is present or not
     let event = await eventsCollection.findOne({_id: ObjectId(eventId)})
     if (!event) {
         throw {statusCode: 404, error:`No such event with id: ${eventId}`}
     }
 
-    let insertLike = await eventsCollection.updateOne({_id:ObjectId(eventId)}, {$addToSet: {likes: newLike}});
-    if (insertLike.modifiedCount === 0) {
-        throw {statusCode: 500, error: "Internal Server Error: Unable to add the Like to the Event"};
-    }
-
-    let getUpdatedEvent = await eventsCollection.findOne({_id: ObjectId(eventId)});
-    if(!getUpdatedEvent){
-      throw {statusCode: 404, error: "Unable to get event after adding the Like"};
-    }
-
-    // Recalculate the likes and dislikes for the event and return.
-    numberOfLikeObject = getUpdatedEvent.likes.length;
-    let likeCount = 0, dislikesCount = 0;
-    getUpdatedEvent.likes.forEach(likeObject => {
-        if (likeObject.value === "like") {
-            likeCount += 1;
+    //Check if any like or a dislike is present for a given userId and eventID
+    let val = await getLikesDislikesByUserId(userId,eventId);
+    if(val==="none")
+    {
+        let insertLike = await eventsCollection.updateOne({_id:ObjectId(eventId)}, {$addToSet: {likes: newLike}});
+        if (insertLike.modifiedCount === 0) {
+            throw {statusCode: 500, error: "Internal Server Error: Unable to add the Like to the Event"};
         }
-      });
-    dislikesCount = numberOfLikeObject - likeCount;
+    }
+    else if(val==="like")
+    {   
 
-    return {like: likeCount, dislike: dislikesCount};
+        if(value==="like")
+        {
+            let removeLike = await removeLikesDislikes(userId,eventId);
+        }
+        else if(value==="dislike")
+        {
+            let update = await updateLikesDislikes(userId,eventId,value);
+            return "dislikeoverlike";
+        }
+        else
+        {
+            throw {statusCode: 500, error: "Invalid value for like-dislike"};
+        }
+    }
+    else if(val==="dislike")
+    {
+        if(value==="dislike")
+        {
+            let removeLike = await removeLikesDislikes(userId,eventId);
+        }
+        else if(value==="like")
+        {
+            let update = await updateLikesDislikes(userId,eventId,value);
+            return "likeoverdislike";
+        }
+        else
+        {
+            throw {statusCode: 500, error: "Invalid value for like-dislike"};
+        }
+    }
 };
 
 const getLikesDislikes = async (eventId) => {
@@ -85,7 +106,61 @@ const getLikesDislikes = async (eventId) => {
     return {like: likeCount, dislike: dislikesCount};
 };
 
+const getLikesDislikesByUserId = async (userId,eventId) =>{
+    await validationFunctions.idValidator(userId);
+    await validationFunctions.idValidator(eventId);
+    
+    let eventsCollection = await events();
+    
+    let eventData = await eventsCollection.findOne({_id: ObjectId(eventId)});
+    if (eventData==null) {
+        throw {statusCode: 404, error: "This event doesn't exist"}
+    }
+
+    let likes_obj_arr = eventData.likes;
+    let value;
+    for(let i=0;i<likes_obj_arr.length;i++)
+    {
+        if(likes_obj_arr[i].user_id.toString()===userId)
+        {
+            value=likes_obj_arr[i].value;
+            return value;
+        }
+    }
+    return "none";
+}
+
+//removes whole like object from of the given userId for an eventId
+const removeLikesDislikes = async (userId,eventId) =>{
+    await validationFunctions.idValidator(userId);
+    await validationFunctions.idValidator(eventId);
+    let eventsCollection = await events();
+
+    let eventData = await eventsCollection.findOne({_id: ObjectId(eventId)});
+    if (eventData==null) {
+        throw {statusCode: 404, error: "This event doesn't exist"}
+    }
+
+    let removeLikeDislike = await eventsCollection.updateOne({_id:ObjectId(eventId)},{$pull:{likes:{user_id:ObjectId(userId)}}});
+    if (removeLikeDislike.modifiedCount === 0) {
+        throw {statusCode: 500, error: "Internal Server Error: Unable to remove Like to the Event"};
+    }
+}
+
+const updateLikesDislikes = async (userId,eventId,value) =>{
+
+    let eventsCollection = await events();
+    let uddatelike = await eventsCollection.update({_id:ObjectId(eventId)},{"$set":{"likes.$[elemX].value":value}},{"arrayFilters":[{"elemX.user_id":ObjectId(userId)}]});
+    if(uddatelike.modifiedCount===0)
+    {
+        throw {statusCode: 500, error: "Internal Server Error: Unable to update Like to the Event"};
+    }
+}
+
 module.exports = {
     createlike,
-    getLikesDislikes
+    getLikesDislikes,
+    getLikesDislikesByUserId,
+    removeLikesDislikes,
+    updateLikesDislikes
 };
